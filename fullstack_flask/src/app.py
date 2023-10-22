31c86f81defdd398bd68ad7687263461b12b6176
Huajun Zeng <hjzeng@gmail.com>
2023-10-22 12:02:50 -0700

    Add MySql

--- fullstack_flask/src/app.py
+++ fullstack_flask/src/app.py
@@ -8,15 +8,18 @@ from .models import *
 
 
 app = Flask(__name__)
+app.register_blueprint(chat_bp)
 
 if not os.getenv("RUNNING_IN_PRODUCTION"):
     app.logger.setLevel(logging.DEBUG)
+    db_uri = 'mysql://root@localhost/chatbot_app'
+else:
+    app.logger.setLevel(logging.INFO)
 
-app.register_blueprint(chat_bp)
+    db_path = os.path.join(os.path.dirname(__file__), 'app.db')
+    print("DB path:", db_path)
+    db_uri = 'sqlite:///{}'.format(db_path)
 
-db_path = os.path.join(os.path.dirname(__file__), 'app.db')
-print("DB path:", db_path)
-db_uri = 'sqlite:///{}'.format(db_path)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db.init_app(app)
 
