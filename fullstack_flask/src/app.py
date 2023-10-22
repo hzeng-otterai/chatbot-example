c9505415e0905365743d4ec6b20fe81d0566293a
Huajun Zeng <hjzeng@gmail.com>
2023-10-22 12:53:08 -0700

    fix

--- fullstack_flask/src/app.py
+++ fullstack_flask/src/app.py
@@ -12,13 +12,13 @@ app.register_blueprint(chat_bp)
 
 if not os.getenv("RUNNING_IN_PRODUCTION"):
     app.logger.setLevel(logging.DEBUG)
-    db_uri = 'mysql://root@localhost/chatbot_app'
-else:
-    app.logger.setLevel(logging.INFO)
 
     db_path = os.path.join(os.path.dirname(__file__), 'app.db')
     print("DB path:", db_path)
     db_uri = 'sqlite:///{}'.format(db_path)
+else:
+    app.logger.setLevel(logging.INFO)
+    db_uri = 'mysql://root@localhost/chatbot_app'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db.init_app(app)
