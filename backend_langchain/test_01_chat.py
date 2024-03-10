2adab90e96980f3c63662a63d1882ef8f6ba7655
Huajun Zeng <hjzeng@gmail.com>
2024-03-09 23:22:58 -0800

    Refine

--- backend_langchain/test_01_chat.py
+++ backend_langchain/test_01_chat.py
@@ -1,13 +1,27 @@
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 
+from langchain_core.messages import HumanMessage, SystemMessage
+
+
 llm1 = ChatOpenAI()
 llm2 = ChatAnthropic(model_name="claude-2.0")
 
-question = "What would be a good company name for a company that makes colorful socks?"
+messages = [
+    SystemMessage(content="You're a helpful assistant"),
+    HumanMessage(content="Suggest some good names for a shoe company."),
+]
 
-result1 = llm1.invoke(question)
-result2 = llm2.invoke(question)
-
+result1 = llm1.invoke(messages)
 print("Result from OpenAI:", result1.content)
+
+result2 = llm2.invoke(messages)
 print("Result from Anthropic:", result2.content)
+
+print("Streaming result from OpenAI:")
+for chunk in llm1.stream(messages):
+    print(chunk.content, end="", flush=True)
+
+print("Streaming result from Anthropic:")
+for chunk in llm2.stream(messages):
+    print(chunk.content, end="", flush=True)
